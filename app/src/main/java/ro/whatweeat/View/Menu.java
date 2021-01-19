da2e13bf2d9b6dfeb20ec45c4baf84514bbec64a
Daniela Boncu <daniella.boncu@gmail.com>
2021-01-19 11:32:23 +0200

    Added a new activity.

---
+++ app/src/main/java/ro/whatweeat/View/Menu.java
@@ -0,0 +1,14 @@
+package ro.whatweeat.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
